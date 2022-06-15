e3863587676bce19a026bb17ec562e1ad958b815
prudhvi malladi <prudhvi malladi@Prudhvi-Malladi>
2022-06-15 17:54:47 +0530

    Second

---
+++ Modifier/src/com/pattern/Pattern.java
@@ -0,0 +1,17 @@
+package com.pattern;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		int i,j;
+		int n=10;
+		for(i = n ; i>0; i--) {
+			for(j=1; j<=i;j++) {
+				System.out.print(j);
+			}
+			System.out.println("");
+		}		
+
+	}
+
+}
